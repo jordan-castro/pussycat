f711c70fb29e75315e013354e6e6d5df34bbe714
James Garfield <jordan@grupojvm.com>
2021-11-16 14:19:14 -0500

    contracts/PetiteCatERC20: Removed uneeded import

--- contracts/PetiteCatERC20.sol
+++ contracts/PetiteCatERC20.sol
@@ -2,7 +2,6 @@
 
 pragma solidity ^0.8.0;
 
-import "../contracts/token/ERC20/ERC20.sol";
 import "../contracts/security/Pausable.sol";
 import "../contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "../contracts/utils/AccessControl.sol";
